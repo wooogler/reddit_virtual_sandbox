06240b2ea23c930d2cf642d0a9a8700a72eb4c09
coder-mina <iamhappy537@gmail.com>
2020-11-23 23:39:40 +0900

    server: get_and_save_posts() DRAFT

--- server/server/data_handler/views.py
+++ server/server/data_handler/views.py
@@ -9,6 +9,7 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
 from .models import Submission, Comment
+from .reddit_handler import RedditHandler
 from .serializers import SubmissionSerializer, CommentSerializer 
 
 # class SubmissionViewSet(viewsets.ModelViewSet):
@@ -22,18 +23,14 @@ from .serializers import SubmissionSerializer, CommentSerializer
 class DataHandlerView(viewsets.ViewSet):
     
     def list(self, request):
-        """
-        http method: GET
-        url: /data/ 
+        """GET /data/ 
         """
         queryset = Submission.objects.all()
         serializer = SubmissionSerializer(queryset, many=True)
         return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
-        """
-        http method: GET
-        url: /data/{pk}
+        """GET /data/{pk}
         """
         queryset = Submission.objects.all()
         res = get_object_or_404(queryset, pk=pk)
@@ -42,5 +39,25 @@ class DataHandlerView(viewsets.ViewSet):
         return Response(serializer.data)
 
     @action(methods=['post'], detail=False)
-    def set_password(self, request):
-        return Response({'set': 1})
+    def save_to_database(self, request):
+        """POST /data/save_to_database/
+        """
+        # TODO: Check if request is valid format.
+        
+        subreddit = request.data['subreddit']
+        start_time = request.data['start_time']
+        end_time = request.data['end_time']
+        try:
+            start_ts = math.ceil(datetime.strptime(args.start_date, "%Y-%m-%d-%H:%M:%S").timestamp())
+            end_ts = math.ceil(datetime.strptime(args.end_date, "%Y-%m-%d-%H:%M:%S").timestamp())
+            
+            reddit = reddit_handler.RedditHandler()
+            status = reddit.get_and_save_posts(
+                subreddit=subreddit, start_at=start_ts, end_at=end_ts,
+                submission_table=Submission, comment_table=Comment
+            )
+            status = 200 if status == True else 500
+            return Response({'status': status})
+
+        except:
+            return Response({'status': 500})
