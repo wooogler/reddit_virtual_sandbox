8ac510106f36afcd4ddd8c6fc4092b52d09bd357
wooogler <leesang627@gmail.com>
2021-01-26 12:18:47 +0000

    staged changes

--- client/src/static/svg/index.ts
+++
@@ -1,6 +0,0 @@
-export {ReactComponent as CloseIcon} from './close.svg';
-export {ReactComponent as PlusIcon} from './plus.svg';
-export {ReactComponent as InfoIcon} from './info.svg';
-export {ReactComponent as ExpandIcon} from './expand.svg';
-export {ReactComponent as CollapseIcon} from './collapse.svg';
-export {ReactComponent as ExternalIcon} from './external.svg';
