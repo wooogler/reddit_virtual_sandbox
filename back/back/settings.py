fcfc50aca99528de362946dbc57f5a5d3ad1510e
woogler <leesang627@gmail.com>
2021-07-07 12:44:20 +0900

    change sqlite to postgresql

--- back/back/settings.py
+++ back/back/settings.py
@@ -98,26 +98,26 @@ WSGI_APPLICATION = 'back.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', ''),
+        'USER': os.getenv('DB_USER', ''),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': '5432',
+        'TEST': {
+            'NAME': os.getenv('DB_TEST', ''),
+        },
     }
 }
-
-# DATABASES = {
-# 	'default' : {
-# 		'ENGINE': 'django.db.backends.postgresql',
-# 		'NAME': os.getenv('DB_NAME', ''),
-# 		'USER': os.getenv('DB_USER', ''),
-# 		'PASSWORD': os.getenv('DB_PASSWORD', ''),
-# 		'HOST': os.getenv('DB_HOST', ''),
-# 		'PORT': '5432',
-# 		'TEST': {
-# 			'NAME': os.getenv('DB_TEST', ''),
-# 		},
-# 	}
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
