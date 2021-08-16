3bc46a83ac7e471a51db14fd3f04bd7155814523
woogler <leesang627@gmail.com>
2021-08-16 17:48:10 +0900

    delete duplicate

--- back/back/settings.py
+++ back/back/settings.py
@@ -123,20 +123,6 @@ INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', ''),
-        'USER': os.getenv('DB_USER', ''),
-        'PASSWORD': os.getenv('DB_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
-        'PORT': '5432',
-        'TEST': {
-            'NAME': os.getenv('DB_TEST', ''),
-        },
-    }
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
